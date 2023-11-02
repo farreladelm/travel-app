ed26323d678aa01d0b85c0367502d988ea8d3cf3
Farrel Adel Mohammad <farrel.adel@gmail.com>
2023-11-02 18:26:26 +0700

    finish all content except footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Camp from "@/components/Camp";
+import Feature from "@/components/Feature";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-    <main>
-      <h1 className='text-xl font-bold text-teal-500'>Hello World!</h1>
-    </main>
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Feature />
+      <GetApp />
+    </>
   )
 }
